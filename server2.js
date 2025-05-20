d3b7252d305fe27d9618e0b3bc9eb75c038339c5
jaamz04 <jamesarpilang04@gmail.com>
2025-05-21 00:17:30 +0700

    try rana

---
+++ server2.js
@@ -0,0 +1,39 @@
+// const express = require("express");
+// const http = require('http');
+// const socketIo = require('socket.io');
+// const cors = require("cors");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server, {
+//     cors: {
+//         origin: '*',
+//         methods:['GET', 'POST'],
+//     }
+// });
+// app.use(cors('*'));
+
+// app.use(express.json());
+
+// app.post('/send', (req,res)=>{
+//     const message = req.body.message
+//     console.log(message);
+
+//     io.emit('pushNotification', {
+//         message
+//     })
+//     res.status(200).send({
+//         message: 'Sent Successfully'
+//     })
+
+//     io.on('connection', (socket)=>{
+//         console.log("Connected")
+//         socket.on('Disconnected', ()=>{
+//             console.log('Client Disconnected')
+//         })
+//     })
+// })
+
+// server.listen(3000, ()=>{
+//     console.log('Server is running on 3000')
+// })
