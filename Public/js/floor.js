604294731ee0659f7c72ce6339f32c8deb7a0cd6
scyyythe <caneteangel187@gmail.com>
2025-05-21 19:23:12 +0800

    bins

--- Public/js/floor.js
+++ Public/js/floor.js
@@ -1,7 +1,24 @@
-const floorSelect = document.getElementById('floorSelect');
-const floorImage = document.getElementById('floorImage');
+  const floorSelect = document.getElementById('floorSelect');
+  const floorImage = document.getElementById('floorImage');
+  const floorTitle = document.getElementById('floorTitle');
 
-floorSelect.addEventListener('change', function () {
-  const selectedFloor = this.value;
-  floorImage.src = `../image/Floor Plan ${selectedFloor}.png`;
-});
+  floorSelect.addEventListener('change', function () {
+    const selectedFloor = this.value;
+
+    // Update floor image
+    floorImage.src = `/image/Floor Plan ${selectedFloor}.png`;
+
+    // Update title
+    floorTitle.textContent = `Floor ${selectedFloor}`;
+
+    // Show the corresponding floor group
+    const floorGroups = document.querySelectorAll('.floor-group');
+    floorGroups.forEach(group => {
+      if (group.getAttribute('data-floor') === selectedFloor) {
+        group.style.display = 'block';
+      } else {
+        group.style.display = 'none';
+      }
+    });
+  });
+
